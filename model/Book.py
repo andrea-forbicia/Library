ce991be523e54036f496b4aec92a4763e9db2e54
Andrea Forbicia <80457490+andrea-forbicia@users.noreply.github.com>
2022-05-24 16:00:33 +0200

    add README.md file

--- model/Book.py
+++ model/Book.py
@@ -1,7 +1,8 @@
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author, shelf):
         self.title = title
         self.author = author
+        self.shelf = shelf
 
     def get_title(self):
         return self.title
@@ -12,11 +13,13 @@ class Book:
     def to_string(self):
         return f"{self.get_title()}, {self.get_author()}"
 
-    def get_shelf(self, library):
+    def get_floor(self):
         pass
 
-    def get_bookshelf(self, library):
+    def get_bookshelf(self):
         pass
 
-    def get_floor(self, library):
-        pass
+    def get_shelf(self):
+        return self.shelf
+
+
